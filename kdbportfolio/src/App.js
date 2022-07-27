8b63c9b7e1ebfd8a322f71a61f4b83d3ca99d4e1
Demon <kristyndcb@gmail.com>
2022-07-27 17:38:55 -0600

    app.js done w routes

--- kdbportfolio/src/App.js
+++ kdbportfolio/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React from "react";
+import Layout from "./components/Layout/Layout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import Portfolio from "./views/Portfolio";
+import Resume from "./views/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/portfolio" element={<Portfolio />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/resume" element={<Resume />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </>
   );
 }
 
