15415efe28f86c1a9c90a4e349bdd2ec4891d0cf
Demon <kristyndcb@gmail.com>
2022-08-04 10:45:44 -0600

    linkedin and github added

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,16 +1,7 @@
-import React, { useState } from "react";
-import MiniContact from "../card/MiniContact";
+import React from "react";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 export default function Footer() {
-  const [openContact, setOpen] = useState(false);
-  const onBtnClick = (event) => {
-    const temState = !openContact;
-    setOpen(temState);
-  };
-  const closeCard = (event) => {
-    setOpen(false);
-  };
   return (
     <>
       <footer>
@@ -33,20 +24,5 @@ export default function Footer() {
         </div>
       </footer>
     </>
-    // <footer>
-    //   <div className="social-link">
-    //     <a href={linkedInUrl} target="_blank" rel="noreferrer">
-    //       LinkedIn
-    //     </a>
-    //     <a href={githubUrl} target="_blank" rel="noreferrer">
-    //       GitHub
-    //     </a>
-    //   </div>
-    //   {/* <p>© 2022 Kristyn D. ❤️ Website Productions</p> */}
-    //   {/* <div className="mini-contact-frame">
-    //     <a onClick={onBtnClick}>Get in Touch🌱</a>
-    //     {openContact && <MiniContact closeCard={closeCard} />}
-    //   </div> */}
-    // </footer>
   );
 }
