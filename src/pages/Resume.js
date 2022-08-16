8492dba418c8e382995a2e8f63ecfa09b5ccfd78
Demon <kristyndcb@gmail.com>
2022-08-16 07:56:01 -0600

    fixed cv download button and updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -11,10 +11,10 @@ export default function Resume() {
       <div className="resume-frame">
         <div className="title-frame">
           <h1 className="title">Resume</h1>
-          <a className="downloadBtn" href={resume} download>
-            Download CV
-          </a>
         </div>
+        <a className="downloadBtn" href={resume} download>
+          Download CV
+        </a>
         <Eduction />
         <Skill />
       </div>
